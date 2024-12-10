863dff70f4c0a47062eea96da951efaaf9afcf93
shimo23333 <shimo23333@gmail.com>
2024-12-11 06:27:14 +0800

    建立linebot回應範例

--- api/index.py
+++ api/index.py
@@ -1,15 +1,68 @@
-from flask import Flask
+from flask import Flask, request, abort
+from dotenv import load_dotenv
+import os
+
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+#改成新的了~~~(.evn)
+#需要有 官方帳號的 TOKEN 和 SECRET
+load_dotenv()
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+LINE_CHANNEL_TOKEN = os.getenv("LINE_CHANNEL_TOKEN")
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+configuration = Configuration(access_token=LINE_CHANNEL_SECRET)
+handler = WebhookHandler(LINE_CHANNEL_TOKEN)
 
-@app.route('/about')
-def about():
-    return 'About'
 
-@app.route('/line')
-def line():
-    return 'LINE TEST'
+#LINE 官方帳號的 webhook 網址輸入後，的驗證回傳函示。略過
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+#run 自動回應使用者輸入的文字
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
+
+if __name__ == "__main__":
+    app.run()
