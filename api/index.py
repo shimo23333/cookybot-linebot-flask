ba635e8dbea44ef055d70fc25a5344037c71b438
shimo23333 <shimo23333@gmail.com>
2024-12-11 22:14:10 +0800

    openAI測試

--- api/index.py
+++ api/index.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, abort
 import os
+from openai import OpenAI
+from PIL import Image
 
 
 from linebot.v3 import (
@@ -30,8 +32,14 @@ from linebot.v3.webhooks import (
 
 app = Flask(__name__)
 
+# LINE 密鑰
 configuration = Configuration(access_token=os.getenv("LINE_CHANNEL_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+# 輸入你的 OpenAI API 密鑰
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
 
 @app.route("/")
 def home():
@@ -57,26 +65,44 @@ def callback():
 
     return 'OK'
 
+
+# 根據食材生成晚餐食譜
+def generate_recipe(ingredients):
+    food_description = ", ".join(ingredients)
+    recipe_prompt = f"請根據這些食材：{food_description}，生成一份晚餐食譜，150字以內簡短介紹。"
+
+    response_recipe = client.chat.completions.create(
+        model="gpt-4-turbo", 
+        messages=[
+            {"role": "system", "content": "你是一位大廚，根據食材創建美味的晚餐食譜。"},
+            {"role": "user", "content": recipe_prompt}
+        ]
+    )
+    recipe_text = response_recipe.choices[0].message.content
+    return recipe_text
+
+
 #run 自動回應使用者輸入的文字
 @line_handler.add(MessageEvent, message=TextMessageContent)
 def line_handler_message(event):
     with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
         
-        if event.message.text == "先度瑞拉":
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text="再度你媽")]
-                )
+        # 用逗號分割成一個陣列
+        textParts = event.message.text.split(",")
+        
+        # 把陣列喂給openAI
+        openAiResponse = generate_recipe(textParts)
+        
+        # 傳送文字回應
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=openAiResponse)] 
             )
-        else:
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text=event.message.text)] 
-                )
-            )
+        )
+            
+
              
 #run 自動回應使用者傳送的圖片
 @line_handler.add(MessageEvent, message=ImageMessageContent)
@@ -97,8 +123,8 @@ def line_handler_message(event):
                 ] 
             )
         )
+    
         
-            
 
 if __name__ == "__main__":
     app.run()
